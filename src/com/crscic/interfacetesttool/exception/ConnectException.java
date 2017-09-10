325291407625065cfa670a95719a6d698824b4f4
shinji2k <ken_829@sohu.com>
2017-09-10 23:59:55 +0800

    1.补上上一个包忘记上传的ConnectException；2.新增实体类，准备对象化数据xml，为解耦做准备

---
+++ src/com/crscic/interfacetesttool/exception/ConnectException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.crscic.interfacetesttool.exception;
+
+/**
+ * 仅用来中断程序
+ * @author ken_8
+ * 2017年9月10日 下午11:09:07
+ */
+public class ConnectException extends Exception
+{
+	private static final long serialVersionUID = -3319490080953863019L;
+
+}
